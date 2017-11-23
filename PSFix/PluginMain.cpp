ec246c62ac6df163faa5275ac088b0772839f98b
Ivan Neeson <zzymyn@gmail.com>
2017-11-23 22:21:32 +1100

    Put everything into the plugin.

---
+++ PSFix/PluginMain.cpp
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+// The plugin does nothing, all the fun happens in DLLMain.
+
+extern "C" __declspec(dllexport) std::int32_t PluginMain(const char* caller, const char* selector, void* message)
+{
+	return 0;
+}
