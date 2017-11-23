d943b7768b0adb06d8cee0e00f4feea7f2e0b0c8
Ivan Neeson <zzymyn@gmail.com>
2017-11-23 17:03:03 +1100

    Add dllmain.

---
+++ PSFixPlugin/Main.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
+{
+	BOOLEAN bSuccess = TRUE;
+
+	switch (nReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hDllHandle);
+		break;
+
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+
+	return bSuccess;
+}
